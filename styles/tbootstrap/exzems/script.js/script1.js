bdcf5d03facbcf844031a549e8dd3e994985760d
dancer138 <lenovotab2020new@gmail.com>
2025-11-05 18:59:50 +0300

    daefdesf
    fsfesf

---
+++ styles/tbootstrap/exzems/script.js/script1.js
@@ -0,0 +1,18 @@
+
+let favoriteFood = "пицца";
+favoriteFood ="суши";
+const planet = "земля";
+const birthday = "12 мая";
+let age = 15;
+let $ = 1;
+let _ = 15;
+let n = 123;
+n = 12.345;
+let isSunny = true;
+let isRaining = false;
+if (isRaining) {
+    console.log("Можно идти гуляфть!");
+}
+let age;
+alert(age); // dsdtltn "undefined"
+
