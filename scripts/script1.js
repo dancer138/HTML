8073af7799da0ffd3dd3b7968da3b3b8e0912b89
dancer138 <lenovotab2020new@gmail.com>
2025-11-10 18:56:10 +0300

    jawaSkript

---
+++ scripts/script1.js
@@ -0,0 +1,72 @@
+
+let favoriteFood = "пицца";
+favoriteFood ="суши";
+const planet = "земля";
+const birthday = "12 мая";
+let age =  "Вы принимаете файлы  cookie!"
+let $ = 1;
+let _ = 15;
+let n = 123;
+n = 12.345;
+let isSunny = true;
+let isRaining = false;
+if (isRaining) {
+    console.log("Можно идти гулять!");
+}
+alert(age); // dsdtltn "undefined"
+
+let box = null;
+let item ={
+    title: "книга",
+    pages:200,
+    availeble: true
+}
+let temperature=25;
+if(temperature>25){
+    console.log('|на улице тепло')
+}
+
+
+let number = 5;
+
+if(number < 0){
+    console.log('плохое число(цифра)')
+} else {
+    console.log('хорошое число(цифра)')
+}
+
+
+console.log('test javascript file');
+
+
+let grade=9;
+if(grade>=9){
+    console.log('отлично')
+}
+else if(grade>=7){
+    console.log('хорошо')
+}
+else if(grade>=5){
+    console.log('удовлетворительно')
+}
+else{
+    console.log('нужно подтянуться')
+
+}
+
+let temp=20;
+if(temp>=18){
+    console.log('тепло')
+}
+else if(temp>=13){
+    console.log('нормально')
+}
+else if(temp>=5){
+    console.log('прохладно')
+}
+else{
+    console.log('холодно')
+
+}
+let isOnlain = true;
+let status = isOnlain ? "A" :"B";
