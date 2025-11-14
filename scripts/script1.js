646540dc0168744ae51ec4e7b9cecf9d33f6f725
dancer138 <lenovotab2020new@gmail.com>
2025-11-14 19:58:57 +0300

    в
    в

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,29 +1,25 @@
+    let age =  "Вы принимаете файлы  cookie!";
+    alert(age); // выводит "undefined"
+    let password;
+    do {
+        password = prompt("Введите пароль:");
+    } while (password !== "1234");
+    alert ("подтверждено");
+    console.log("пароль принят!");
 
 let favoriteFood = "пицца";
-favoriteFood ="суши";
 const planet = "земля";
 const birthday = "12 мая";
-let age =  "Вы принимаете файлы  cookie!"
 let $ = 1;
-let _ = 15;
 let n = 123;
-n = 12.345;
 let isSunny = true;
 let isRaining = false;
 if (isRaining) {
     console.log("Можно идти гулять!");
 }
-alert(age); // dsdtltn "undefined"
-
-let box = null;
-let item ={
-    title: "книга",
-    pages:200,
-    availeble: true
-}
 let temperature=25;
 if(temperature>25){
-    console.log('|на улице тепло')
+    console.log('на улице тепло')
 }
 
 
@@ -37,22 +33,6 @@ if(number < 0){
 
 
 console.log('test javascript file');
-
-
-let grade=9;
-if(grade>=9){
-    console.log('отлично')
-}
-else if(grade>=7){
-    console.log('хорошо')
-}
-else if(grade>=5){
-    console.log('удовлетворительно')
-}
-else{
-    console.log('нужно подтянуться')
-
-}
 
 let temp=20;
 if(temp>=18){
@@ -68,8 +48,6 @@ else{
     console.log('холодно')
 
 }
-let isOnlain = true;
-let status = isOnlain ? "A" :"B";
 for(let i = 1; i <= 5; i++) {
     console.log("Повтор " + i);
 
@@ -93,28 +71,19 @@ while (i <= 11){;
     console.log("Шаг" + i);
     i++;
 }
-
-
-    let password;
-    do {
-        password = prompt("Введите пароль:");
-    } while (password !== "1234");
-    alert ("подтверждено");
-    console.log("пароль принят!");
-
     let fruits = ["яблоко", "бананы","груша"];
     for (let fruit of fruits) {
         console.log(fruit);
     }
-    let animals = ["лиса", "влад","сокол"];
-    for (let animals of animals) {
-        console.log(animals);
-    }
-    for (let i = 1; i <= 10; i++){
-        console.log('3 x ${i} = ${3 * i}');
-    }
-    let fruitWord = "Анас";
-    let fruitWordCount = 0;
-    for(let letter of fruitWord.toLower)
- 
+    // let animals = ["лиса", "влад","сокол"];
+    // for (let animals of animals) {
+    //     console.log(animals);
+    // }
+    // for (let i = 1; i <= 10; i++){
+    //     console.log('3 x ${i} = ${3 * i}');
+    // }
+    // let fruitWord = "Анас";
+    // let fruitWordCount = 0;
+    // for(let letter of fruitWord.toLower);
     
+
